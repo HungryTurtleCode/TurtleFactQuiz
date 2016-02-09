4a69138e8fde06b3625aad68157b10dcbb2b3570
Adrian <adimandevelopment@gmail.com>
2016-02-09 23:19:59 +0000

    populated JSON mock data

--- js/factory/dataService.js
+++ js/factory/dataService.js
@@ -61,24 +61,84 @@
      * location and the size of the turtles
      *
      */
-    var correctAnswers = [1, 3, 0];
+    var correctAnswers = [1, 2, 3, 0, 2, 0, 3, 2, 0, 3];
 
     var quizQuestions  = [
         {
+            type: "text",
+            text: "How much can a loggerhead weigh?",
+            possibilities: [
+                {
+                    answer: "Up to 20kg"
+                },
+                {
+                    answer: "Up to 115kg"
+                },
+                {
+                    answer: "Up to 220kg"
+                },
+                {
+                    answer: "Up to 500kg"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text",
+            text: "What is the typical lifespan of a Green Sea Turtle?",
+            possibilities: [
+                {
+                    answer: "150 years"
+                },
+                {
+                    answer: "10 years"
+                },
+                {
+                    answer: "80 years"
+                },
+                {
+                    answer: "40 years"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
             type: "image",
-            text: "This is the questions text 0",
+            text: "Which of these is the Alligator Snapping Turtle?",
             possibilities: [
                 {
-                    answer: "https://placeholdit.imgix.net/~text?txtsize=33&txt=800%C3%97800&w=800&h=800"
+                    answer: "https://c1.staticflickr.com/3/2182/2399413165_bcc8031cac_z.jpg?zz=1"
                 },
                 {
-                    answer: "https://placeholdit.imgix.net/~text?txtsize=33&txt=800%C3%97800&w=800&h=800"
+                    answer: "http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg"
                 },
                 {
-                    answer: "https://placeholdit.imgix.net/~text?txtsize=33&txt=800%C3%97800&w=800&h=800"
+                    answer: "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg"
                 },
                 {
-                    answer: "https://placeholdit.imgix.net/~text?txtsize=33&txt=800%C3%97800&w=800&h=800"
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Alligator_snapping_turtle_-_Geierschildkr%C3%B6te_-_Alligatorschildkr%C3%B6te_-_Macrochelys_temminckii_01.jpg"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "image",
+            text: "Which of these is the Green Turtle?",
+            possibilities: [
+                {
+                    answer: "http://www.what-do-turtles-eat.com/wp-content/uploads/2014/10/Sea-Turtles-Habitat.jpg"
+                },
+                {
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kemp's_Ridley_sea_turtle_nesting.JPG"
+                },
+                {
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Alligator_snapping_turtle_-_Geierschildkr%C3%B6te_-_Alligatorschildkr%C3%B6te_-_Macrochelys_temminckii_01.jpg"
+                },
+                {
+                    answer: "http://assets.worldwildlife.org/photos/163/images/carousel_small/SCR_290360hawskbill-why-matter-LG.jpg?1345565532"
                 }
             ],
             selected: null,
@@ -86,19 +146,19 @@
         },
         {
             type: "text",
-            text: "This is the questions text 1",
+            text: "Where does the Kemp's Ridley Sea Turtle live?'",
             possibilities: [
                 {
-                    answer: "Hello this is question 0"
+                    answer: "Tropical waters all around the world"
                 },
                 {
-                    answer: "Hello this is question 1"
+                    answer: "Eastern Australia"
                 },
                 {
-                    answer: "Hello this is question 2"
+                    answer: "Coastal North Atlantic"
                 },
                 {
-                    answer: "Hello this is question 3"
+                    answer: "South pacific islands"
                 }
             ],
             selected: null,
@@ -106,19 +166,99 @@
         },
         {
             type: "text",
-            text: "This is the questions text 2",
+            text: "What is the most common turtle in US waters?",
             possibilities: [
                 {
-                    answer: "Hello this is question 0"
+                    answer: "Loggerhead turtle"
                 },
                 {
-                    answer: "Hello this is question 1"
+                    answer: "Leatherback turtle"
                 },
                 {
-                    answer: "Hello this is question 2"
+                    answer: "Hawksbill Turtle"
                 },
                 {
-                    answer: "Hello this is question 3"
+                    answer: "Alligator Snapping Turtle"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text",
+            text: "What is the largest sea turtle on earth?",
+            possibilities: [
+                {
+                    answer: "Eastern Snake Necked Turtle"
+                },
+                {
+                    answer: "Olive Ridley Sea Turtle"
+                },
+                {
+                    answer: "Kemp's Ridley Sea Turtle'"
+                },
+                {
+                    answer: "Leatherback"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "image",
+            text: "Which of these is the Olive Ridley Turtle?",
+            possibilities: [
+                {
+                    answer: "http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg"
+                },
+                {
+                    answer: "http://assets.worldwildlife.org/photos/163/images/carousel_small/SCR_290360hawskbill-why-matter-LG.jpg?1345565532"
+                },
+                {
+                    answer: "http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg"
+                },
+                {
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kemp's_Ridley_sea_turtle_nesting.JPG"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text",
+            text: "How Heavy can a leatherback turtle be?",
+            possibilities: [
+                {
+                    answer: "900kg"
+                },
+                {
+                    answer: "40kg"
+                },
+                {
+                    answer: "110kg"
+                },
+                {
+                    answer: "300kg"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text",
+            text: "Which of these turtles are herbivores?",
+            possibilities: [
+                {
+                    answer: "Loggerhead Turtle"
+                },
+                {
+                    answer: "Hawksbill Turtle"
+                },
+                {
+                    answer: "Leatherback Turtle"
+                },
+                {
+                    answer: "Green Turtle"
                 }
             ],
             selected: null,
@@ -128,76 +268,76 @@
 
     var turtlesData = [
         {
-            type: "African Side Neck Turtle",
-            image_url: "https://c2.staticflickr.com/8/7151/6566869583_0274c54e12_b.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
+            type: "Green Turtle",
+            image_url: "http://www.what-do-turtles-eat.com/wp-content/uploads/2014/10/Sea-Turtles-Habitat.jpg",
+            locations: "Tropical and subtropical oceans worldwide",
+            size: "Up to 1.5m and up to 300kg",
+            lifespan: "Over 80 years",
+            diet: "Herbivore",
+            description: "The green turtle is a large, weighty sea turtle with a wide, smooth carapace, or shell. It inhabits tropical and subtropical coastal waters around the world and has been observed clambering onto land to sunbathe. It is named not for the color of its shell, which is normally brown or olive depending on its habitat, but for the greenish color of its skin. There are two types of green turtles—scientists are currently debating whether they are subspecies or separate species—including the Atlantic green turtle, normally found off the shores of Europe and North America, and the Eastern Pacific green turtle, which has been found in coastal waters from Alaska to Chile."
+        },
+        {
+            type: "Loggerhead Turtle",
+            image_url: "http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg",
+            locations: "Tropical and subtropical oceans worldwide",
+            size: "90cm, 115kg",
+            lifespan: "More than 50 years",
+            diet: "Carnivore",
+            description: "Loggerhead turtles are the most abundant of all the marine turtle species in U.S. waters. But persistent population declines due to pollution, shrimp trawling, and development in their nesting areas, among other factors, have kept this wide-ranging seagoer on the threatened species list since 1978. Their enormous range encompasses all but the most frigid waters of the world's oceans. They seem to prefer coastal habitats, but often frequent inland water bodies and will travel hundreds of miles out to sea."
+        },
+        {
+            type: "Leatherback Turtle",
+            image_url: "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg",
+            locations: "All tropical and subtropical oceans",
+            size: "Up to 2m, up to 900kg",
+            lifespan: "45 years",
+            diet: "Carnivore",
+            description: "Leatherbacks are the largest turtles on Earth, growing up to seven feet (two meters) long and exceeding 2,000 pounds (900 kilograms). These reptilian relics are the only remaining representatives of a family of turtles that traces its evolutionary roots back more than 100 million years. Once prevalent in every ocean except the Arctic and Antarctic, the leatherback population is rapidly declining in many parts of the world. While all other sea turtles have hard, bony shells, the inky-blue carapace of the leatherback is somewhat flexible and almost rubbery to the touch. Ridges along the carapace help give it a more hydrodynamic structure. Leatherbacks can dive to depths of 4,200 feet (1,280 meters)—deeper than any other turtle—and can stay down for up to 85 minutes."
+        },
+        {
+            type: "Hawksbill Sea Turtle",
+            image_url: "http://assets.worldwildlife.org/photos/163/images/carousel_small/SCR_290360hawskbill-why-matter-LG.jpg?1345565532",
+            locations: "Tropical Coastal areas around the world",
+            size: "Over 1m, 45-68kg",
+            lifespan: "30-50 Years",
+            diet: "Carnivore",
             description: "Dolor possimus voluptas hic aliquam rem doloremque minus maiores accusantium? Laborum perferendis harum blanditiis quod quia? Aspernatur sunt et fuga delectus ab rem excepturi. Ipsa quibusdam facere consequuntur magnam vitae? Consectetur consectetur necessitatibus beatae delectus quibusdam in! Est nobis omnis iusto illum fugiat maxime! Neque fugiat reiciendis sequi corrupti minima facere distinctio aliquam est voluptatibus. Sint incidunt soluta atque ducimus."
         },
         {
             type: "Alligator Snapping Turtle",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Alligator_snapping_turtle_-_Geierschildkr%C3%B6te_-_Alligatorschildkr%C3%B6te_-_Macrochelys_temminckii_01.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
+            locations: "Along the Atlantic Coast of USA",
+            size: "around 60cm, up to 100kg",
+            lifespan: "20-70 years",
+            diet: "Carnivore",
+            description: "The prehistoric-looking alligator snapping turtle is the largest freshwater turtle in North America and among the largest in the world. With its spiked shell, beaklike jaws, and thick, scaled tail, this species is often referred to as the 'dinosaur of the turtle world.' Found almost exclusively in the rivers, canals, and lakes of the southeastern United States, alligator snappers can live to be 50 to 100 years old. Males average 26 inches (66 centimeters) in shell length and weigh about 175 pounds (80 kilograms), although they have been known to exceed 220 pounds (100 kilograms). The much smaller females top out at around 50 pounds (23 kilograms)."
         },
         {
-            type: "Common Snapping Turtle",
-            image_url: "https://www.marshall.edu/herp/images/SNAPPERHEAD.JPG",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descriptsodfhjlskdfjsdlkfjsklfjskfjslfjslkjing"
+            type: "Kemp's Ridley Sea Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kemp's_Ridley_sea_turtle_nesting.JPG",
+            locations: "Coastal areas of the North Atlantic",
+            size: "65cm, up to 45kg",
+            lifespan: "Around 50 years",
+            diet: "Omnivore",
+            description: "The Kemp’s ridley turtle is the world’s most endangered sea turtle, and with a worldwide female nesting population roughly estimated at just 1,000 individuals, its survival truly hangs in the balance. Their perilous situation is attributed primarily to the over-harvesting of their eggs during the last century. And though their nesting grounds are protected and many commercial fishing fleets now use turtle excluder devices in their nets, these turtles have not been able to rebound. For this reason, their nesting processions, called arribadas, make for especially high drama. During an arribada, females take over entire portions of beaches, lugging their big bodies through the sand with their flippers until they find a satisfying spot to lay their eggs. Even more riveting is the later struggle to the ocean of each tiny, vulnerable hatchling. Beset by predators, hatchlings make this journey at night, breaking out of their shells using their caruncle, a single temporary tooth grown just for this purpose."
         },
         {
-            type: "Box Turtle",
-            image_url: "http://www.corhs.org/uploaded/20110925-130952_DSC_3378small.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
+            type: "Olive Ridley Turtle",
+            image_url: "http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg",
+            locations: "Tropical coastal areas around the world",
+            size: "70cm, 45kg",
+            lifespan: "50 years",
+            diet: "Omnivore",
+            description: "The olive ridley turtle is named for the generally greenish color of its skin and shell, or carapace. It is closely related to the Kemp’s ridley, with the primary distinction being that olive ridleys are found only in warmer waters, including the southern Atlantic, Pacific and Indian Oceans. Olive and Kemp’s ridleys are the smallest of the sea turtles, weighing up to 100 pounds (45 kilograms) and reaching only about 2 feet (65 centimeters) in shell length. The olive ridley has a slightly smaller head and smaller shell than the Kemp’s."
         },
         {
             type: "Eastern Snake Necked Turtle",
             image_url: "https://c1.staticflickr.com/3/2182/2399413165_bcc8031cac_z.jpg?zz=1",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
-        },
-        {
-            type: "Diamondback Terrapin",
-            image_url: "http://ncpedia.org/sites/default/files/diamondbackTerrapin1.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
-        },
-        {
-            type: "Indian Peacock Softshell Turtle",
-            image_url: "http://www.turtlesurvival.org/storage/images/species/chitra_indica4.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
-        },
-        {
-            type: "Eastern River Cooter",
-            image_url: "https://c1.staticflickr.com/9/8294/7513837434_ab9cdb7432.jpg",
-            locations: "Atlantic Coast of USA",
-            size: "Up to 1m",
-            lifespan: "15 years",
-            diet: "Food",
-            description: "This is a fucking descripting"
-        },
+            locations: "Eastern Australia",
+            size: "Up to 30cm",
+            lifespan: "25 years",
+            diet: "Carnivore",
+            description: "Snake-necked turtles, as the name suggests, have an unusually long neck. Their necks may be up to 60 percent of their carapace length. Their carapace is oval and flattened, usually dark-brown to black measuring up to 11 inches (27.5 cm) in length. Scutes are shed as the animals grow. The males have a longer, thicker tail than females and a concave plastron. They are excellent swimmers; they have large, webbed feet with sharp claws used to tear apart food."
+        }
     ];
 })();
